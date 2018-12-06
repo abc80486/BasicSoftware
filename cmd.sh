ef9374f0c69f366e1e134f06482f308b898e86eb
xiangbin <abc80486@163.com>
2018-12-06 15:48:07 +0800

    12.6.15.47

--- cmd.sh
+++ cmd.sh
@@ -1,9 +1,9 @@
+cd /usr/share/nginx/html/website/
 echo "时间："
 date "+%Y,%B,%d,%A."
 echo -n ""
 echo "---------------------------"
 echo -n ""
-cd /usr/share/nginx/html/website/
 git add .
 git commit -m "qqserver"
 git pull
